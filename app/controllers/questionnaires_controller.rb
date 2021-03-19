0bb8ce8e5b851adb5a712bfdae3e1c62807d1023
John Paul Villamor <jpvillam@mailfence.com>
2021-03-19 17:08:01 -0400

    pass a string to question.alternatives

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -195,8 +195,7 @@ class QuestionnairesController < ApplicationController
       end
       question.size = '50, 3' if question.is_a? Criterion
       question.size = '50, 3' if question.is_a? Cake
-      # FIXME: Below: Do not assume 0-5 only!
-      question.alternatives = Set.new(@questionnaire.min_question_score..@questionnaire.max_question_score) if question.is_a? Dropdown
+      question.alternatives = (@questionnaire.min_question_score..@questionnaire.max_question_score).to_a.join("|") if question.is_a? Dropdown
       question.size = '60, 5' if question.is_a? TextArea
       question.size = '30' if question.is_a? TextField
       begin
